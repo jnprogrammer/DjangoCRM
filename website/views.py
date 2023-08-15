5eee8d45f461d828a0a5f1bda91481c4886cc8e6
Josh <joshuanparker@gmail.com>
2023-08-14 22:12:30 -0400

    Now records can be added.

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -67,4 +67,17 @@ def delete_record(request,pk):
         return redirect('home')
     else:
         messages.success(request, "Log in first to do that ")
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You need to log in first")
         return redirect('home')
