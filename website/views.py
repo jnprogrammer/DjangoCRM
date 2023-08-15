6f5baa1aef04ffb387bd00570699267dc320fb5e
Josh <joshuanparker@gmail.com>
2023-08-14 21:12:59 -0400

    Added single record page

--- website/views.py
+++ website/views.py
@@ -49,3 +49,12 @@ def register_user(request):
      return render(request, 'register.html', {'form':form})
 
 
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #look up records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to see this")
+        return redirect('home')
+
